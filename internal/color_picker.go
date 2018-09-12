d831832f415e4856ec5d3aa7093d28e73aca0080
Gabriel Sanches <g@lonemaker.com>
2018-09-12 19:16:30 -0300

    Return a new color when picking

--- internal/color_picker.go
+++ internal/color_picker.go
@@ -9,21 +9,21 @@ import (
 // ColorPicker is a hash map that loops through it
 // sequentially to pick a color.
 type ColorPicker struct {
-	colors []*color.Color
-	mu     *sync.Mutex
-	i      int
+	attrs []color.Attribute
+	mu    *sync.Mutex
+	i     int
 }
 
 // NewColorPicker creates a filled color picker.
 func NewColorPicker() *ColorPicker {
 	return &ColorPicker{
-		colors: []*color.Color{
-			color.New(color.FgRed),
-			color.New(color.FgGreen),
-			color.New(color.FgYellow),
-			color.New(color.FgBlue),
-			color.New(color.FgMagenta),
-			color.New(color.FgCyan),
+		attrs: []color.Attribute{
+			color.FgRed,
+			color.FgGreen,
+			color.FgYellow,
+			color.FgBlue,
+			color.FgMagenta,
+			color.FgCyan,
 		},
 		mu: &sync.Mutex{},
 	}
@@ -34,13 +34,12 @@ func NewColorPicker() *ColorPicker {
 func (cp *ColorPicker) Pick() *color.Color {
 	defer cp.mu.Unlock()
 	cp.mu.Lock()
-	c := cp.colors[cp.index()]
-	return c
+	return color.New(cp.attrs[cp.index()])
 }
 
 func (cp *ColorPicker) index() int {
 	defer func() { cp.i++ }()
-	if cp.i >= len(cp.colors) {
+	if cp.i >= len(cp.attrs) {
 		cp.i = 0
 	}
 	return cp.i
